7b979019af55762a00fb862dbdef99b6ca220de3
Brad Flavel <bradflavel@hotmail.com>
2026-03-25 21:03:55 +1000

    add Commander with create command, arugment and template option

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log("hello world")
+import { Command } from "commander"
+
+const addProgram = new Command()
+
+addProgram
+    .name('project-scaffolder')
+    .description('CLI to create new scaffolding')
+    .version('0.1.0');
+
+addProgram.command('create')
+    .description('Creates the scaffolding')
+    .argument('<project-name>', 'project name')
+    .option('-t, --template <type>', 'choose type')
+    .action((name, options) => {
+        console.log(name, options)
+    })
+
+
+addProgram.parse()
+
